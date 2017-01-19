e99b618c84de574c4150d3638accfcf2c7dfbcd9
Lukas Trumm <lukas.trumm@centrum.cz>
2017-01-19 11:10:19 +0100

    Add show hide animations

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular
-  .module('encrypter', ['ngMessages', 'ngMaterial'])
+  .module('encrypter', ['ngMessages', 'ngMaterial', 'ngAnimate'])
   .controller('EncrypterController', EncrypterController)
   .config(function($mdThemingProvider) {
     $mdThemingProvider
